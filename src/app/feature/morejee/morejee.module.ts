b155d47dd75a6f8a005baeb1dbd3b07ecc5c5f6d
leon-github <liwang.pu@gmail.com>
2019-06-05 00:11:30 +0800

    添加基础按钮样式

--- src/app/feature/morejee/morejee.module.ts
+++ src/app/feature/morejee/morejee.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { ReactiveFormsModule } from '@angular/forms';
 //mat modules
+import {MatButtonModule} from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MorejeeRoutingModule } from './morejee-routing.module';
 import { AssetUploaderComponent } from './components/asset-uploader/asset-uploader.component';
@@ -14,6 +15,7 @@ import { TextureToMaterialComponent } from './components/texture-to-material/tex
     CommonModule,
     TranslateModule,
     ReactiveFormsModule,
+    MatButtonModule,
     MatSnackBarModule,
     MorejeeRoutingModule
   ]
