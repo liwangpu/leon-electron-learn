fd38d345e4420979c078b1d8dcaf73917ba4005e
leon <liwang.pu@gmail.com>
2019-06-04 15:40:28 +0800

    2

--- src/app/feature/morejee/components/asset-uploader/asset-uploader.component.ts
+++ src/app/feature/morejee/components/asset-uploader/asset-uploader.component.ts
@@ -1,5 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { ElectronDialogService } from '@app/core';
+import * as path from "path";
+import * as fs from 'fs';
+class AssetList {
+  dataMap: { [key: string]: DataMap };
+  dependencies: { [key: string]: DataMap };
+}
+
+class DataMap {
+  package: string;
+  name: string;
+  class: string;
+  tags: object;
+  dependencies: { [key: string]: AssetDependency }
+}
+
+class AssetDependency {
+  package: string;
+  name: string;
+  class: string;
+  level: number;
+  localPath: string;
+  fileAssetId: string;
+  objId: string;
+}
 
 @Component({
   selector: 'morejee-asset-uploader',
@@ -8,7 +32,8 @@ import { ElectronDialogService } from '@app/core';
 })
 export class AssetUploaderComponent implements OnInit {
 
-  _projectDir="";
+  _projectDir = "";
+
   constructor(protected electDialogSrv: ElectronDialogService) {
 
   }//constructor
@@ -19,7 +44,24 @@ export class AssetUploaderComponent implements OnInit {
 
   selectProjectDir() {
     let dirs = this.electDialogSrv.showOpenDialog({ properties: ['openDirectory'] });
-    if (dirs && dirs.length > 0)
-      this._projectDir = dirs[0];
+    if (!dirs || dirs.length == 0) return;
+    this._projectDir = dirs[0];
+    let assetListPath = path.join(this._projectDir, "Saved", "AssetMan", "assetlist.txt");
+    fs.exists(assetListPath, exist => {
+      if (!exist) return;
+      this.analyzeAssetFromConfig(assetListPath);
+    });
+    // this.analyzeAssetFromConfig(assetListPath);
+    console.log('assetListPath', assetListPath);
   }//selectProjectDir
+
+  analyzeAssetFromConfig(assetListPath: string) {
+    fs.readFile(assetListPath, {encoding: 'utf8'}, (err, data) => {
+      if (err) return;
+      // let sss=JSON.parse(data); 
+      console.log(111, data);
+    });
+  }//analyzeAssetFromConfig
+
+
 }
